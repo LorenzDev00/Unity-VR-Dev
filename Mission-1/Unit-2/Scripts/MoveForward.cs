57881e4b433de9f24ce835ffb34334038ae89df9
Lorenzo Bignami <95931734+LorenzDev00@users.noreply.github.com>
2024-01-31 23:57:32 +0100

    Create MoveForward.cs

---
+++ Mission-1/Unit-2/Scripts/MoveForward.cs
@@ -0,0 +1,16 @@
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 40.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
